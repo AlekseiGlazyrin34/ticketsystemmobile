84de4bfecd398d4897e884e87c17ad93e4e616b0
Aleksei <Aleksei@DESKTOP-P2NHSKP>
2025-05-08 23:54:13 +0300

    c

--- pages/CreateRequest.js
+++ pages/CreateRequest.js
@@ -37,7 +37,7 @@ const CreateRequest = () => {
         body: JSON.stringify({
           problemName,
           room,
-          priorityName: priority,
+          priority: priority,
           description,
         }),
       }));
